e1c9cb74133006c89262dc155e7d25ca87d0739f
feitongTang <feitong@umich.edu>
2022-05-14 22:26:10 -0400

    Add Exercise 035

---
+++ 035.cpp
@@ -0,0 +1,30 @@
+/*
+    link: https://leetcode.com/problems/search-insert-position/
+*/
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int low_idx = 0;
+        int up_idx = nums.size() - 1;
+        int cur_idx = (low_idx + up_idx) / 2;
+        
+        while (cur_idx >= low_idx && cur_idx <= up_idx) {
+            if (nums[cur_idx] == target) {
+                return cur_idx;
+            }
+            else if (nums[cur_idx] < target) {
+                low_idx = cur_idx + 1;
+                cur_idx = (low_idx + up_idx) / 2;
+            } else {
+                up_idx = cur_idx - 1;
+                cur_idx = (low_idx + up_idx) / 2;
+            }
+        }
+        return low_idx;
+    }
+};
