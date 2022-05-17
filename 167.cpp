7eac6cce9e4458f35f0f299d9c02a51882dc5bba
feitongTang <feitong@umich.edu>
2022-05-16 22:52:56 -0400

    Add Exercise 167

---
+++ 167.cpp
@@ -0,0 +1,25 @@
+/*
+    link: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+*/
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int begin_ptr = 0, end_ptr = numbers.size() - 1;
+        while (begin_ptr < end_ptr) {
+            if (numbers[begin_ptr] + numbers[end_ptr] > target) {
+                end_ptr--;
+            } else if (numbers[begin_ptr] + numbers[end_ptr] == target){
+                break;
+            } else {
+                begin_ptr++;
+            }
+        }
+        vector<int> result{begin_ptr + 1, end_ptr + 1};
+        return result;
+    }
+};
