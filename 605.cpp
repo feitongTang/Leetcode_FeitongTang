15c800deae2e329bdc35a51576558a09647f3446
feitongTang <feitong@umich.edu>
2022-06-02 11:36:46 -0400

    Another solution to 605

--- 605.cpp
+++ 605.cpp
@@ -34,4 +34,25 @@ public:
         n -= (flowerbed.size() + 1 - flower_idx) / 2 - 1;
         return (n <= 0);
     }
+
+    /*
+        another solution from online
+        it inserts 0 in the beginning and end, which simplifies the calculation a lot.
+        also it uses another way to count
+    */
+
+   bool canPlaceFlowers_online(vector<int>& flowerbed, int n) {
+        flowerbed.insert(flowerbed.begin(),0);
+        flowerbed.push_back(0);
+        for(int i = 1; i < flowerbed.size()-1; ++i)
+        {
+            if(flowerbed[i-1] + flowerbed[i] + flowerbed[i+1] == 0)
+            {
+                --n;
+                ++i;
+            }
+                
+        }
+        return n <=0;
+    }
 };
