d0c5826ab7ebb6e939f89ada5d4fd27be469c9ad
feitongTang <feitong@umich.edu>
2022-06-12 09:58:23 -0400

    Add another solution to Exercise 704

--- 704.cpp
+++ 704.cpp
@@ -2,6 +2,17 @@
     link: https://leetcode.com/problems/binary-search/
 */
 
+/*
+    comment:
+    a typical binary search algorithm
+*/
+
+/*
+    status:
+    Runtime: 7.19%
+    Memory Usage: 92.13%
+*/
+
 #include <vector>
 
 using namespace std;
@@ -27,4 +38,21 @@ public:
         }
         return -1;
     }
+
+    /*
+        another solution from online by lone_02wolf (https://leetcode.com/problems/binary-search/discuss/1456130/C%2B%2B-Simple-and-Fast-Binary-Search)
+        the idea is the same, but the code is shorter and simpler
+    */
+
+   int search_online(vector<int>& nums, int target) {
+        int n = nums.size()-1;
+        int low = 0, high = n;
+        while( low <= high){
+            int mid = (low + high)/2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target) high = mid -1;
+            else low = mid + 1;
+        }
+        return -1;
+  }
 };
